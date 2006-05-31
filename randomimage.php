767126d8a3fdfd95ac376dd89fa22ae8f9d7a0a7
Justin Watt <justincwatt@gmail.com>
2006-05-30 19:30:30 -0700

    Add the ability to selectively filter images by post category
    Also:
    * added instructions to the configuration interface

--- randomimage.php
+++ randomimage.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: randomimage
-Version: 2.0
+Version: 2.1
 Plugin URI: http://justinsomnia.org/2005/09/random-image-plugin-for-wordpress/
 Description: Display a random image that links back to the post it came from
 Author: Justin Watt
@@ -17,6 +17,10 @@ INSTRUCTIONS
    (make sure to replace the square brackets [] above with angle brackets <>)
 
 CHANGELOG
+
+2.1
+added the ability to selectively filter images by post category
+added instructions to the configuration interface
 
 2.0
 created administrative interface for managing options
@@ -79,6 +83,13 @@ function get_randomimage_options()
 {
     $randomimage_options = get_option('randomimage_options');
 
+    // for forward compatibility, make sure new category_filter option gets set as an array
+    if (isset($randomimage_options) && !isset($randomimage_options["category_filter"]))
+    {
+        $randomimage_options["category_filter"] = array();
+        add_option('randomimage_options', $randomimage_options);
+    }
+
     // init default options if options aren't found
     if (!$randomimage_options) 
     {
@@ -89,57 +100,63 @@ function get_randomimage_options()
                                      "number_of_images"     => 1, 
                                      "image_attributes"     => "", 
                                      "inter_image_html"     => "<br /><br />",
-                                     "image_src_regex"      => "");
+                                     "image_src_regex"      => "",
+                                     "category_filter"      => array());
         add_option('randomimage_options', $randomimage_options);
     }
 
     return $randomimage_options;
 }
 
+
 // generate configuration page
 function randomimage_configuration_page()
 {
+    $randomimage_options = get_randomimage_options();
+
+    // if form has been submitted, save values
+    if ( isset($_POST['submit']) )
+    {
+        // correct for empty image number
+        if ($_POST['number_of_images'] < 1)
+        {
+            $_POST['number_of_images'] = 1;
+        }
+
+        // correct for posts and pages being deselected
+        if (!isset($_POST['show_images_in_posts']) && !isset($_POST['show_images_in_pages']))
+        {
+            $_POST['show_images_in_posts'] = "on";
+        }
+
+        if (!is_array($_POST['category_filter']))
+        {
+            $_POST['category_filter'] = array();
+        }
+
+        // create array of new options
+        $randomimage_options = array(
+            "show_post_title"      => $_POST['show_post_title'],
+            "show_alt_caption"     => $_POST['show_alt_caption'],
+            "show_images_in_posts" => $_POST['show_images_in_posts'],
+            "show_images_in_pages" => $_POST['show_images_in_pages'],
+            "number_of_images"     => $_POST['number_of_images'],
+            "image_attributes"     => stripslashes($_POST['image_attributes']),
+            "inter_image_html"     => stripslashes($_POST['inter_image_html']),
+            "image_src_regex"      => stripslashes($_POST['image_src_regex']),
+            "category_filter"      => $_POST['category_filter']
+        );
+        update_option('randomimage_options', $randomimage_options);
+    }
+
 ?>
 
 <div class="wrap">
 <h2>Random Image Settings</h2>
 
-<?php
-
-$randomimage_options = get_randomimage_options();
-
-// if form has been submitted, save values
-if ( isset($_POST['submit']) )
-{
-    // correct for empty image number
-    if ($_POST['number_of_images'] < 1)
-    {
-        $_POST['number_of_images'] = 1;
-    }
-
-    // correct for posts and pages being deselected
-    if (!isset($_POST['show_images_in_posts']) && !isset($_POST['show_images_in_pages']))
-    {
-        $_POST['show_images_in_posts'] = "on";
-    }
-
-    // create array of new options
-    $randomimage_options = array(
-        "show_post_title"      => $_POST['show_post_title'],
-        "show_alt_caption"     => $_POST['show_alt_caption'],
-        "show_images_in_posts" => $_POST['show_images_in_posts'],
-        "show_images_in_pages" => $_POST['show_images_in_pages'],
-        "number_of_images"     => $_POST['number_of_images'],
-        "image_attributes"     => stripslashes($_POST['image_attributes']),
-        "inter_image_html"     => stripslashes($_POST['inter_image_html']),
-        "image_src_regex"      => stripslashes($_POST['image_src_regex'])
-    );
-    update_option('randomimage_options', $randomimage_options);
-}
-
-?>
-
 <form method="post" action="">
+
+<p><strong>Instructions:</strong> Use the following options to configure how you want the Random Image plugin to appear. The sample image below will reflect the changes you make. When you're satisfied, add <code>&lt?php randomimage(); ?&gt</code> to your index.php or sidebar.php template file located in /<em>path</em>/<em>to</em>/<em>wordpress</em>/wp-content/themes/<em>theme-name</em>/
 
 <div style="clear: both;padding-top:10px;">
 <label style="float:left;width:250px;text-align:right;padding-right:6px;" for="show_post_title">Show post title above image?</label>
@@ -158,12 +175,33 @@ if ( isset($_POST['submit']) )
 
 <div style="clear: both;padding-top:10px;">
 <label style="float:left;width:250px;text-align:right;padding-right:6px;padding-top:7px;" for="image_attributes">Optional attributes for each <code>&lt;img&gt;</code> tag:</label>
-<div style="float:left;"><input type="text" id="image_attributes" name="image_attributes" <?php if (isset($randomimage_options["image_attributes"])) print "value='" . stripslashes(htmlspecialchars($randomimage_options["image_attributes"], ENT_QUOTES)) . "'"; ?>/> e.g. <code>style="width:200px;"</code></div>
+<div style="float:left;"><input type="text" id="image_attributes" name="image_attributes"  style="width:200px;" <?php if (isset($randomimage_options["image_attributes"])) print "value='" . stripslashes(htmlspecialchars($randomimage_options["image_attributes"], ENT_QUOTES)) . "'"; ?>/> e.g. <code>style="width:200px;"</code></div>
+</div>
+
+<div style="clear: both;padding-top:10px;">
+<label style="float:left;width:250px;text-align:right;padding-right:6px;padding-top:7px;">Limit by categories:<br />(leave unchecked for all)</label>
+<div style="float:left;">
+
+<div style='overflow:auto;height:6em;width:200px;background-color:#efefef;border:1px solid #b2b2b2;padding:2px 0 0 3px;'>
+<?php
+
+    // create WordPress-style category multi-select list
+    global $wpdb;
+    $categories = $wpdb->get_results("SELECT cat_ID, cat_name
+                                      FROM $wpdb->categories
+                                      ORDER BY cat_name");
+    foreach ($categories as $category) {
+        print "<label style='display:block;' for='category-$category->cat_ID'><input type='checkbox' value='$category->cat_ID' name='category_filter[]' id='category-$category->cat_ID'" . (in_array( $category->cat_ID, $randomimage_options["category_filter"] ) ? ' checked="checked"' : "") . " />" .  wp_specialchars($category->cat_name) . "</label>\n";
+    }
+?>
+</div>
+
+</div>
 </div>
 
 <div style="clear: both;padding-top:10px;">
 <label style="float:left;width:250px;text-align:right;padding-right:6px;padding-top:7px;" for="image_src_regex">Regex to match against the <code>&lt;img&gt;</code> <code>src</code>:</label>
-<div style="float:left;"><input type="text" id="image_src_regex" name="image_src_regex" <?php if (isset($randomimage_options["image_src_regex"])) print "value='" . stripslashes(htmlspecialchars($randomimage_options["image_src_regex"], ENT_QUOTES)) . "'"; ?>/> e.g. <code>images</code></div>
+<div style="float:left;"><input type="text" id="image_src_regex" name="image_src_regex" style="width:200px;" <?php if (isset($randomimage_options["image_src_regex"])) print "value='" . stripslashes(htmlspecialchars($randomimage_options["image_src_regex"], ENT_QUOTES)) . "'"; ?>/> e.g. <code>images</code></div>
 </div>
 
 <div style="clear: both;padding-top:10px;text-align:center;">
@@ -180,6 +218,7 @@ if ( isset($_POST['submit']) )
 
 
 
+
 <?php
 }
 
@@ -191,7 +230,8 @@ function randomimage($show_post_title  = true,
                      $show_alt_caption = true, 
                      $image_src_regex  = "",
                      $post_type        = "posts",
-                     $inter_image_html = "<br /><br />")
+                     $inter_image_html = "<br /><br />",
+                     $category_filter  = "")
 {
     // get access to wordpress' database object
     global $wpdb;
@@ -208,7 +248,15 @@ function randomimage($show_post_title  = true,
         $show_alt_caption = $randomimage_options['show_alt_caption'];        
         $image_src_regex  = $randomimage_options['image_src_regex'];        
         $inter_image_html = $randomimage_options['inter_image_html'];
-        
+
+        if (!is_array($randomimage_options['category_filter']))
+        {
+            $randomimage_options['category_filter'] = array();
+        }
+
+        // convert category filter array into a comma-separated list
+        $category_filter  = implode(",", $randomimage_options['category_filter']);
+
         if ($randomimage_options['show_images_in_posts'] == true && $randomimage_options['show_images_in_pages'] == false)
         {
             $post_type = "posts";
@@ -222,7 +270,7 @@ function randomimage($show_post_title  = true,
              $post_type = "both";
         }
     }
-    
+
     // select the post_type sql for both post pages (post_status = 'static') 
     // and posts (AND post_status = 'publish')
     // or for just pages or for just posts (the default)
@@ -241,13 +289,31 @@ function randomimage($show_post_title  = true,
         $post_type_sql = "AND post_status = 'publish'";
     }
 
+    // assuming $category_filter is a comma separated list of category ids,
+    // modify query to join with post2cat table to select from only the chosen categories
+    if ($category_filter != "")
+    {
+        $category_filter_join  = "LEFT JOIN $wpdb->post2cat ON $wpdb->posts.ID = $wpdb->post2cat.post_id";
+        $category_filter_sql   = "AND $wpdb->post2cat.category_id IN ($category_filter)";   
+        $category_filter_group = "GROUP BY $wpdb->posts.ID";
+    }
+    else
+    {   
+        $category_filter_join  = "";
+        $category_filter_sql   = "";
+        $category_filter_group = "";
+    }
+
     // query records that contain img tags, ordered randomly
     // do not select images from password protected posts
-    $sql = "SELECT * 
-            FROM $wpdb->posts 
+    $sql = "SELECT $wpdb->posts.ID, $wpdb->posts.post_title, $wpdb->posts.post_content
+            FROM $wpdb->posts
+            $category_filter_join
             WHERE post_content LIKE '%<img%'
             AND post_password = ''
             $post_type_sql
+            $category_filter_sql
+            $category_filter_group
             ORDER BY rand()";
     $resultset = mysql_query($sql) or die($sql);
 
